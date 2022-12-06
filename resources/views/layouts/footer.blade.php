c8eb951291fd400cacbffd7e3e5c5fa1fdf66328
Tomas Bartu <tomasbartu00@gmail.com>
2022-12-06 21:03:22 +0100

    add footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,1 @@
+<div><p class="text-center m-0 bg-white opacity-50">Copyright <i class="bi bi-c-circle"></i> 2022 Učební kartičky, Výhradně pro vzdělávací účely.</p></div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -41,6 +41,9 @@
             @yield('content')
         </div>
     </div>
+    <div class="flex-grow-0 flex-shrink-1">
+        @include('layouts.footer')
+    </div>
 </div>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
